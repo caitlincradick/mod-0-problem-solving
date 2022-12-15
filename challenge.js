7ef32d5da30be7998a88bfbc3cbb3ce51bbb0208
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-14 21:52:21 -0800

    add code for the cchallenge

--- challenge.js
+++ challenge.js
@@ -1,2 +1,7 @@
 // 1. Given an array of strings, return only the strings that have exactly 4 characters.
 
+var animals = ["badger", "emu", "elephant", "lion", "cat"]
+    for(i=0; i < animals.length; i++) 
+        if (animals[i].length === 4) {
+        console.log(animals[i]);
+        }
