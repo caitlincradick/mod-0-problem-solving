4ad021ebf6322451cc6c74424fa6544d90861165
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-15 18:48:55 -0800

    add overall go and pseudocode to file

---
+++ challenge_3.js
@@ -0,0 +1,18 @@
+// For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**.
+
+
+// 1. Start with an array of strings with a mix of uppercase and lowercase letters. 
+// Print every word in all lowercase letters in the array
+
+//-create variable of arrays with uppercase/lowercase letters 
+//-for statement 
+//- use toLowerCase
+//- print console.log
+
+
+var crazyString = ["LOL", "omg", "brb", "HAM"];
+for(i=0; i < crazyString.length; i++) {
+    if (crazyString => crazyString.toLowerCase[i])
+    console.log(crazyString[i].toLowerCase());
+}
+
