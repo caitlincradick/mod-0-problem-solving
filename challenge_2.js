7e4118a671330c59c753275b53e5860a4110186d
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-15 18:48:00 -0800

    add overall go and pseudocode to file

--- challenge_2.js
+++ challenge_2.js
@@ -1,15 +1,19 @@
-// // Given an array of strings, return only the words that begin with the letter "t"
-
-// var names = ["Timothy", "Sandra", "Tom", "Jason", "Eric"]
-// for(i=0; )
-
-// return
+// For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**.
 
 // Start with an array of travel destinations. 
 // Print every travel destination in alphabetical order embedded 
 // in a sentence using string interpolation. For example, 
 // if the destination is "New York City", print something 
 // like "The next place I want to visit is New York City!" 
+
+//-get array of travel destinations that will print in alphabetical order 
+//-print them in seperate sentences (still in alphabetical order)
+//- output "The next place I will travel is __"
+
+// //var cities
+// for 
+// sort()
+// 'string interpolation ${travelDestinations[i]'}
 
 var travelDestinations = ["Japan", "Cambiodia", "Brazil", "Ethiopia", "Fiji"]
 for(i=0; i< travelDestinations.length; i++) {
